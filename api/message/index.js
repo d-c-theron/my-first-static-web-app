cc40f7b198ee32431867b612b616f80a93a9cb4f
David Theron <david@sportco.co.za>
2023-02-03 11:17:40 +0200

    added date test 2

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the freshly changed API"
+        text: "Hello from the freshly changed API - 03/02"
     });
 };
